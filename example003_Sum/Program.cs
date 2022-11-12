d5cb907c6fc7f3e6fe30d178d931853dbfd56c22
ismet <ismet.net@mail.ru>
2022-11-12 13:04:35 +0300

    add random

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int numderA = 31;
-int numderB = 5;
+﻿int numderA = new Random() .Next(1,10); // 1 2 3 4 . . . 9
+Console.WriteLine(numderA);
+
+int numderB = new Random() .Next(1,10);
+Console.WriteLine(numderB);
+
 int result = numderA + numderB;
 Console.WriteLine(result);
